ffac17cf6e45578e572b4cc82aa56b7f46b9114f
Yuto Uchimi <momosama1072@gmail.com>
2017-09-27 16:10:40 +0900

    fix CMAKE_INSTALL_PREFIX for pcl and octomap

--- build.sh
+++ build.sh
@@ -6,19 +6,20 @@ THIS_DIR=$(pwd)
 
 git submodule update --init --recursive
 
-export CMAKE_PREFIX_PATH=${THIS_DIR}/devel
+# export CMAKE_PREFIX_PATH=${THIS_DIR}/devel
+export CMAKE_PREFIX_PATH=$HOME/Projects/label_fusion_new/devel
 
 for ext in octomap/octomap pcl; do
   mkdir -p ${THIS_DIR}/${ext}/build
   cd ${THIS_DIR}/${ext}/build
-  cmake .. -DCMAKE_INSTALL_PREFIX:PATH=$CMAKE_PREFIX_PATH
+  cmake .. -DCMAKE_INSTALL_PREFIX:PATH=$CMAKE_PREFIX_PATH -DBUILD_GPU:=true
   make install -j
   cd ${THIS_DIR}
 done
 
-mkdir -p build
-cd build
-cmake ..
-make -j
+# mkdir -p build
+# cd build
+# cmake ..
+# make -j
 
 set +x
